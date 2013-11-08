0606e8391c58ac9c6522d8649ceaba6eb3568de3
Vilmos Nebehaj <v.nebehaj@gmail.com>
2013-11-09 00:22:47 +0100

    Implement command line flags.

--- src/rmq_shovel.erl
+++ src/rmq_shovel.erl
@@ -13,15 +13,25 @@
 main(Args) ->
     OptSpecList =
     [{source, $s, "source", {string, "amqp://guest:guest@localhost:5672/%2f"},
-      "RabbitMQ source AMQP URI."},
+      "RabbitMQ source AMQP URI. Can be used multiple times to consume from "
+      "multiple brokers."},
      {destination, $d, "destination",
-      {string, "amqp://guest:guest@localhost:5673/%2f"}, "RabbitMQ source "
-      "AMQP URI."},
-     {queue, $q, "queue", string, "Queue to consume from. Mandatory."},
-     {prefetch, $p, "prefetch", integer, "Prefetch this number of messages."},
+      {string, "amqp://guest:guest@localhost:5673/%2f"}, "RabbitMQ "
+      "destination AMQP URI."},
+     {src_declare, undefined, "src_declare", string, "Any declaration in "
+      "the source broker. Can be used multiple times."},
+     {dst_declare, undefined, "dst_declare", string, "Any declaration in "
+      "the destination broker. Can be used multiple times."},
+     {queue, $q, "queue", binary, "Queue to consume from. Mandatory."},
+     {prefetch, $n, "prefetch", integer, "Prefetch this number of messages."},
      {reconnect_delay, $r, "reconnect_delay", integer, "Reconnect delay."},
-     {ack_mode, $a, "ack_mode", string, "Acknowledgement mode: no_ack, "
+     {ack_mode, $a, "ack_mode", atom, "Acknowledgement mode: no_ack, "
       "on_publish or on_confirm."},
+     {publish_field, $p, "publish_field", string, "By default Shovel will "
+      "keep message fields (fields in the basic.publish method used to "
+      "re-publish messages, e.g. exchange, routing key, etc) intact. Use "
+      "this option (multiple times if needed) to override any of these "
+      "fields."},
      {config, $f, "config", {string, undefined}, "Configuration file to read "
       "shovel definitions from. This should contain your shovels, as a tuple "
       "ending with a dot for each. If provided, any other command line "
@@ -50,7 +60,38 @@ start_with_configfile(Config) ->
     start_shovels(Shovels).
 
 start_with_cmdline(Props) ->
-    {error, Props}. % TODO
+    Source = proplists:get_all_values(source, Props),
+    Destination = proplists:get_value(destination, Props),
+    SrcDeclare = to_term(proplists:get_all_values(src_declare, Props)),
+    DstDeclare = to_term(proplists:get_all_values(dst_declare, Props)),
+    Queue = proplists:get_value(queue, Props),
+    Fields = to_term(proplists:get_all_values(publish_field, Props)),
+    Prefetch = proplists:get_value(prefetch, Props, 0),
+    AckMode = proplists:get_value(ack_mode, Props, on_confirm),
+    Delay = proplists:get_value(reconnect_delay, Props, 5),
+    Shovel = {shovel, [{sources, [{brokers, Source},
+                                  {declarations, SrcDeclare}]},
+                       {destinations, [{broker, Destination},
+                                       {declarations, DstDeclare}]},
+                       {queue, Queue},
+                       {publish_fields, Fields},
+                       {prefetch_count, Prefetch},
+                       {ack_mode, AckMode},
+                       {reconnect_delay, Delay}
+                      ]},
+    start_shovels([Shovel]).
+
+to_term(List) ->
+    to_term(List, []).
+
+to_term([], Acc) ->
+    Acc;
+
+to_term([H|T], Acc) ->
+    io:format("converting ~p~n", [H]),
+    {ok, Tokens, _} = erl_scan:string(H ++ "."),
+    {ok, Term} = erl_parse:parse_term(Tokens),
+    to_term(T, [Term|Acc]).
 
 start_shovels(Shovels) ->
     io:format("starting shovel(s) ~p~n", [Shovels]),
